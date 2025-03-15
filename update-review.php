75675e06d50e4ed0ca5076ce35242c6f9865bf67
Deec0 <64306289+Deec0@users.noreply.github.com>
2025-03-15 15:55:07 +0600

    Delete update-review.php

--- update-review.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'boot.php';
-$stmt = pdo()->prepare("UPDATE review SET status = :status WHERE id = :id");
-if($stmt->execute(['status' => $_GET['status'], 'id' => $_GET['id']])){
-    echo json_encode(array('status' => '201', 'mess' => 'Успешно'));
-    die;
-}
-echo json_encode(array('status' => '404', 'mess' => 'Пездюк'));
